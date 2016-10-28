983ef4ea11bee7a8feabe655f0b3d2193152eb4a
Joshua Pérez García <alu0100818819@ull.edu.es>
2016-10-28 09:49:16 +0100

    modificando require

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -5,7 +5,7 @@ var fs = require('fs-extended');
 var path = require('path');
 var fs2 = require('fs');
 var iaas = require('gitbook-start-iaas-ull-es-aitor-joshua-samuel');
-var heroku = require('gitbook-start-heroku-ull-es-aitor-joshua-samuel');
+var heroku = require('gitbook-start-heroku-aitor-joshua-samuel');
 
 if (argv.n) {
     var second_path = path.resolve(__dirname, "../template")
